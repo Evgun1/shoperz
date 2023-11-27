a546db1f81a9db9c494e14def42a533d175b7d99
Evgun1 <eugenegavrick@gmail.com>
2023-11-27 14:32:37 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import viteSvgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shoperz/',
   plugins: [react(), viteSvgr()],
 })
